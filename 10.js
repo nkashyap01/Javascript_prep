c5251cfbe8df83d71c96b5e2da2c718f348819b9
neha <nehakashyap1011@gmail.com>
2023-09-11 19:12:58 +0530

    undefined

---
+++ 10.js
@@ -0,0 +1,12 @@
+// // //undefined
+
+// // //null
+
+// // let firstName;
+// // console.log(typeof (firstName));
+
+// var firstName;
+// console.log(typeof firstName);
+
+const firstName="neha";
+console.log(typeof firstName);
