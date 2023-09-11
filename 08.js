a0a12b341d77730ab82e8c5ffdec3cbad796d0d7
neha <nehakashyap1011@gmail.com>
2023-09-11 18:49:51 +0530

    convert number to string and string to number

--- 08.js
+++ 08.js
@@ -1,6 +1,15 @@
-//type operator
+// //type operator
+
+// let age=22;
+// let firstName="Neha";
+// console.log(typeof age);
+// console.log(typeof firstName);
+
+// convert number to string
 
 let age=22;
-let firstName="Neha";
-console.log(typeof age);
-console.log(typeof firstName);
+console.log(typeof(age+""));
+
+//convert string to number
+let mystr=+"34";
+console.log(typeof(mystr));
