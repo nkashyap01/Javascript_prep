a12da2122e40d8a8a2a15c057fca52d2c0f7a453
neha <nehakashyap1011@gmail.com>
2023-09-11 19:20:31 +0530

    about null

--- 10.js
+++ 10.js
@@ -1,12 +1,28 @@
-// // //undefined
+// // // // //undefined
 
-// // //null
+// // // // //null
 
-// // let firstName;
-// // console.log(typeof (firstName));
+// let firstName;
+// console.log(typeof (firstName));
+// firstName="neha";
+// console.log(typeof(firstName), firstName);
 
-// var firstName;
-// console.log(typeof firstName);
 
-const firstName="neha";
-console.log(typeof firstName);
+
+// // // var firstName;
+// // // console.log(typeof firstName);
+
+// // const firstName="neha";
+// // console.log(typeof firstName);
+
+
+
+
+
+//* NUll
+
+let myvariable=null;
+console.log(myvariable);
+console.log(typeof myvariable);
+myvariable="neha";
+console.log(myvariable, typeof myvariable);
