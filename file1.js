78b9b4d96e685fdbd52b32d75c5e07c9e069c380
neha <nehakashyap1011@gmail.com>
2023-09-11 18:36:11 +0530

    learn slice method

--- file1.js
+++ file1.js
@@ -1,11 +1,13 @@
  
  "use strict";
  let firstName="Neha";
- console.log(firstName);
-  firstName="   muskan";
- console.log(firstName);
-console.log(firstName[3]);
-console.log(firstName.length-1);
-firstName.trim();
-firstName=firstName.toUpperCase();
-console.log(firstName);
+//  console.log(firstName);
+//   firstName="   muskan";
+//  console.log(firstName);
+// console.log(firstName[3]);
+// console.log(firstName.length-1);
+// firstName.trim();
+// firstName=firstName.toUpperCase();
+// console.log(firstName);
+let newString=firstName.slice(0,3);
+console.log(newString);
