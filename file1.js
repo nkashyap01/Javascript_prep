8a1e09b63aceeb693e6f71a3770dbb3af38e7e54
neha <nehakashyap1011@gmail.com>
2023-09-11 18:32:10 +0530

    about some function

--- file1.js
+++ file1.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+ 
+ "use strict";
+ let firstName="Neha";
+ console.log(firstName);
+  firstName="   muskan";
+ console.log(firstName);
+console.log(firstName[3]);
+console.log(firstName.length-1);
+firstName.trim();
+firstName=firstName.toUpperCase();
+console.log(firstName);
