dab4772fa63dc5f939805e6cd0a2b1877a846609
neha <nehakashyap1011@gmail.com>
2023-09-11 18:57:11 +0530

    string concatenation

--- 08.js
+++ 08.js
@@ -1,15 +1,31 @@
-// //type operator
+// // // //type operator
 
-// let age=22;
-// let firstName="Neha";
+// // // let age=22;
+// // // let firstName="Neha";
+// // // console.log(typeof age);
+// // // console.log(typeof firstName);
+
+// // // convert number to string
+
+// // let age=22;
+// // console.log(typeof(age+""));
+
+// // //convert string to number
+// // let mystr=+"34";
+// // console.log(typeof(mystr));
+
+
+// // let age=18;
+// // age=String(age);
+// // console.log(typeof age);
+// let age="18";
+// age=Number(age);
 // console.log(typeof age);
-// console.log(typeof firstName);
 
-// convert number to string
 
-let age=22;
-console.log(typeof(age+""));
+// string concatenation
 
-//convert string to number
-let mystr=+"34";
-console.log(typeof(mystr));
+let string1="neha";
+let string2="kashyap";
+// let fullName=string1+" "+string2;
+console.log(string1+" "+string2);
