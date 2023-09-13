1536c3ffa2ce9e577dd1256221d03f9bae7982af
neha <nehakashyap1011@gmail.com>
2023-09-13 19:16:18 +0530

    else if code

---
+++ 18.js
@@ -0,0 +1,21 @@
+//else if
+
+let tempInDegree=15;
+if(tempInDegree<0){
+    console.log("extremely cold outside");
+}else if(tempInDegree<16){
+    console.log("it is cold outside");
+}
+
+else if(tempInDegree<25){
+    console.log("weather is okay");
+}
+else if(tempInDegree<35){
+    console.log("lets go for swim");
+}
+else if(tempInDegree<45){
+    console.log("turn on AC");
+}
+else{
+    console.log("too hot!");
+}
