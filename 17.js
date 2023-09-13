f404a39526ff7af4bd7d44031e3e54dfa67fcb13
neha <nehakashyap1011@gmail.com>
2023-09-13 18:53:57 +0530

    winning game using if else

---
+++ 17.js
@@ -0,0 +1,21 @@
+//nested if else
+// winning number 19
+
+//here user take any input in form of string
+// let winningNumber=19;
+// let userGuess= prompt("Guess a number");
+// console.log(typeof userGuess, userGuess);
+
+
+// if you want to take input in number format add + sign befor prompt or write (int)
+
+let winningNumber=19;
+let userGuess= +prompt("Guess a number");
+console.log(typeof userGuess, userGuess);
+
+if(userGuess===winningNumber) {
+    console.log("your guess is right");
+}
+else {
+    console.log("your guess is wrong");
+}
