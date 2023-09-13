0efdfb941e75803e93504eac2ee7daf8a8a12435
neha <nehakashyap1011@gmail.com>
2023-09-13 18:35:32 +0530

    and or operator

---
+++ 16.js
@@ -0,0 +1,15 @@
+// and or operator
+
+let firstName="Neha";
+let age=22;
+// if(firstName[0]=="N"){
+//     console.log("your name starts with N");
+// }
+
+// if(age>18){
+//     console.log("you are above 18");
+// }
+
+if(firstName[0]=="N"&& age>18){
+    console.log("Name starts with N and above 18");
+}
