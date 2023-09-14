2fea2156e329499e4d9c3c5be06712d194645522
neha <nehakashyap1011@gmail.com>
2023-09-14 18:03:19 +0530

    while &for loop

---
+++ 21.js
@@ -0,0 +1,12 @@
+// //for loop
+
+// for(let i=0;i<=9;i++) {
+//     console.log(i);
+// }
+
+
+for(var i=0;i<=9;i++){
+    console.log(i);
+}
+
+console.log("value of i is",i);
---
+++ 24.js
@@ -0,0 +1,7 @@
+for(let i=1;i<=10;i++){
+    if(i==4){
+        break;
+    }
+    console.log(i);
+}
+console.log("hello there");
