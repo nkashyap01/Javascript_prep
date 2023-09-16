1892e7a96d0da392d0340d4bd93b4795e67d47ab
neha <nehakashyap1011@gmail.com>
2023-09-16 15:39:11 +0530

    primitive vs refrence data types

--- 27.js
+++ 27.js
@@ -4,5 +4,11 @@ let fruits=["apple","mango","grapes"];
 console.log(fruits);
 fruits.push("banana"); 
 console.log(fruits); 
+//for adding elements in first
 fruits.unshift("guava");
 console.log(fruits);
+
+//for removing element at first
+let removedFruit=fruits.shift();
+console.log(fruits);
+console.log("removed fruit is ",removedFruit);
---
+++ 28.js
@@ -0,0 +1,21 @@
+// //primitive vs refrence data types
+// let num1=6;
+// let num2=num1;
+// console.log("value of num1 is",num1);
+// console.log("value of num2 is",num2);
+// num++;
+// console.log("after incrementing num1");
+// console.log("value of num1 is",num1);
+// console.log("value of num2 is",num2);
+
+//refrence types
+//array
+
+let array1=["item1","item2"];
+let array2=array1;
+console.log("array1",array1);
+console.log("array2",array2);
+array1.push("item3");
+console.log("after pushing element to array1");
+console.log("array1",array1);
+console.log("array2",array2);
