be0cac581e94149290e1307a31523d63b6a1cbbe
neha <nehakashyap1011@gmail.com>
2023-09-19 18:35:21 +0530

    const

---
+++ 31.js
@@ -0,0 +1,15 @@
+// //use const for creating array
+
+// here it is showing error because we can't change value of const
+
+// const pi=3.14;
+// pi=12;
+// console.log(pi);
+
+
+const fruits=["apple", "mango"];
+fruits.push("banana");
+console.log(fruits);
+
+
+ 
