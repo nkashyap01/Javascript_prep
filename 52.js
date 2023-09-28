bd8b7b8d47727c1447543abb982b9a7ed1057da8
neha <nehakashyap1011@gmail.com>
2023-09-28 18:38:52 +0530

    rest parameters

--- 52.js
+++ 52.js
@@ -1,11 +1,19 @@
 //default parameters
 
-function addTwo(a,b){
-    if(typeof b ==="undefined"){
-        b=1;
-    }
+// function addTwo(a,b){
+//     if(typeof b ==="undefined"){
+//         b=1;
+//     }
+//     return a+b;
+// }
+
+// const ans=addTwo(4);
+// console.log(ans);
+
+
+function sum(a,b=0) {
     return a+b;
 }
 
-const ans=addTwo(4);
+const ans=sum(4,8);
 console.log(ans);
---
+++ 53.js
@@ -0,0 +1,11 @@
+//rest parameters
+
+function myFunc(a,b,...c) {
+    console.log(`a is ${a}`)
+    console.log(`b is ${b}`)
+    console.log(`c is ${c}`)
+    console.log(`c is`,c)
+
+}
+
+myFunc(3,4,5,6,7,8,9);
