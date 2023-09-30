4b8cd43bf3ad7c3406e16848d89880bdfd1ec81f
neha <nehakashyap1011@gmail.com>
2023-09-30 17:30:34 +0530

    mapping

--- 59.js
+++ 59.js
@@ -1,6 +1,31 @@
 //key value pair
 
-const person =new Map();
-person.set('firstName','Neha');
-person.set('age',22);
-console.log(person);
+// const person =new Map();
+// person.set('firstName','Neha');
+// person.set(1,'one');
+// person.set('age',22);
+// console.log(person);
+// console.log(person.get('firstName'));
+// console.log(person.get(1));
+
+
+// const person=new Map([['firstName','Neha'],['age',20]]);
+// console.log(person);
+
+
+const person1={
+    id:1,
+    firstName:"Neha"
+}
+
+const extraInfo=new Map();
+extraInfo.set(person1,{age:8, gender:"female"});
+//console.log(userInfo);
+
+console.log(person1.id);
+console.log(extraInfo.get(person1));
+
+console.log(extraInfo.get(person1).gender);
+
+
+
