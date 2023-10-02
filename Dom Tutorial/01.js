b5af4115d6f0a1a27a6afb240b8011af26adb736
neha <nehakashyap1011@gmail.com>
2023-10-03 00:45:23 +0530

    select element by id and query selector

---
+++ Dom Tutorial/01.js
@@ -0,0 +1,6 @@
+//DOM
+//select element by id
+const mainHeading=console.log(document.getElementById("main-heading"));
+console.log(mainHeading);
+
+
---
+++ Dom Tutorial/02.js
@@ -0,0 +1,10 @@
+//select element using query selector
+
+const mainHeading=document.querySelector("#main-heading");
+console.log(mainHeading);
+const header2=document.querySelector(".header");
+console.log(header2);
+const formgroup=document.querySelectorAll(".form-group");
+console.log(formgroup);
+ 
+ 
