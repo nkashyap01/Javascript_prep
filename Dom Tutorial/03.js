74e9db7859e1cf20451bd095de3e6b5684b1ae12
neha <nehakashyap1011@gmail.com>
2023-10-03 18:11:25 +0530

    text content property for changing text

---
+++ Dom Tutorial/03.js
@@ -0,0 +1,7 @@
+//change text
+//textContent and innerText
+
+const mainHeading=document.getElementById("main-heading");
+console.log(mainHeading.textContent);
+mainHeading.textContent="This is something else";
+console.log(mainHeading.textContent);
