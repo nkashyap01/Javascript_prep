5ecce40e5872a60f80c77520e948d77a344bffaa
neha <nehakashyap1011@gmail.com>
2023-10-04 10:12:13 +0530

    remove the elements

--- Dom Tutorial/07.js
+++ Dom Tutorial/07.js
@@ -1,6 +1,11 @@
-const newTodoItem=document.createElement("li");
-const newTodoItemText=document.createTextNode("Teach students");
-const todoList=document.querySelector(".todo-list");
-newTodoItem.append(newTodoItemText);
-todoList.append(newTodoItem);
-console.log(newTodoItem);
+// const newTodoItem=document.createElement("li");
+// const newTodoItemText=document.createTextNode("Teach students");
+// const todoList=document.querySelector(".todo-list");
+// newTodoItem.append(newTodoItemText);
+// todoList.append(newTodoItem);
+// console.log(newTodoItem);
+
+
+const todo1=document.querySelector('.todo-list li');
+todo1.remove();
+console.log(todo1);
