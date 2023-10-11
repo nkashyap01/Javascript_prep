dd57c7caaacd10e3aced4e6ac76919ed423bf3fb
neha <nehakashyap1011@gmail.com>
2023-10-11 17:46:42 +0530

    random background changing

--- javascript_basic/setInterval2.js
+++ javascript_basic/setInterval2.js
@@ -1,17 +1,17 @@
-// //setInterval
-
-// console.log("script start");
-// setInterval( ()=>{
-//     console.log(Math.random());
-// },1000);
-// console.log("Script end");
-
 const body= document.body;
-
-setInterval(()=>{
+const button=document.querySelector("button");
+console.log(button);
+const intervalId = setInterval(()=>{
    const red=Math.floor(Math.random()*126);
    const green=Math.floor(Math.random()*126); 
    const blue=Math.floor(Math.random()*126);  
     const rgb=`rgb(${red},${green},${blue})`;
    body.style.background=rgb;
 },1000);
+button.addEventListener("click",()=>{
+   clearInterval(intervalId);
+   button.textContent=body.style.background;
+});
+console.log(intervalId);
+
+ 
