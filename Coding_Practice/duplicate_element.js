2a9b6c71572b41d0903229b20c3d25f42b4986ab
neha <nehakashyap1011@gmail.com>
2023-11-23 21:55:28 +0530

    Javascript program to find duplicate value in an array

---
+++ Coding_Practice/duplicate_element.js
@@ -0,0 +1,7 @@
+// duplicate values in an array
+const arr = [1, 2, 3, 3, 3, 4, 4, 5, 6];
+const result = arr.filter((val, index, array) => {
+  return array.indexOf(val) !== index;
+});
+console.log(arr);
+console.log(result);
--- javascript_basic/callback demo.js
+++ javascript_basic/callback demo.js
@@ -5,5 +5,4 @@ const heading1 = document.querySelector(".heading1");
 setTimeout(() => {
   heading1.textContent = "one";
   heading1.style.color = "red";
-  heading1.style.color = "blue";
 }, 1000);
