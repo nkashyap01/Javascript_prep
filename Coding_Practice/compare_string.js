cf5fe63b40fe15b311b2442b4a666822cfc5b1f8
neha <nehakashyap1011@gmail.com>
2023-11-25 22:04:40 +0530

    Javascript program to compare two strings

---
+++ Coding_Practice/compare_string.js
@@ -0,0 +1,10 @@
+// Javascript program to compare two strings
+
+const string1 = "Hello javascaript";
+const string2 = "Hello JAVASCRIPT";
+
+if (string1.toUpperCase === string2.toUpperCase) {
+  console.log("The string is same");
+} else {
+  console.log("The string is not same");
+}
