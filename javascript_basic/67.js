bf13282dc379a6bd390c29d2bc1e2062cab6f33a
neha <nehakashyap1011@gmail.com>
2023-11-28 23:49:39 +0530

    function creation

--- javascript_basic/67.js
+++ javascript_basic/67.js
@@ -1,17 +1,15 @@
-function func(){
-    counter=0;
-     return function(){
-        if(counter<1){
-        console.log("Hii you called me")
-    counter++;}
-    else{
-
-    console.log("I am already called")
-
+function func() {
+  counter = 0;
+  return function () {
+    if (counter < 1) {
+      console.log("Hii you called me");
+      counter++;
+    } else {
+      console.log("I am already called");
     }
-    }
-   
+  };
 }
-const myfunc=func();
+const myfunc = func();
+myfunc();
 myfunc();
 myfunc();
