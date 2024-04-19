f8f5bd58f34e45d21f796ea11b3a528e1517e252
neha <nehakashyap1011@gmail.com>
2024-04-19 23:19:17 +0530

    practice some array questions

--- 100DaysChallenge/practice.js
+++ 100DaysChallenge/practice.js
@@ -13,3 +13,11 @@ const num = arr.map((val) => {
   }
 });
 console.log(num);
+
+const arr2 = [12,13,12,15];
+const num2 = arr2.find((val)=>{
+  if(val==12) {
+    return val;
+  }
+});
+console.log(num2);
