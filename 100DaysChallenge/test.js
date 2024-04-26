fd581a72e258cdde2984f5af77e8e38e48aea4cb
neha <nehakashyap1011@gmail.com>
2024-04-26 23:44:49 +0530

    made factorial function

---
+++ 100DaysChallenge/test.js
@@ -0,0 +1,15 @@
+function calculateFactorial(number) {
+  if (number < 0) {
+    return "Factorial is not defined for negative numbers.";
+  }
+
+  1;
+  if (number === 0) {
+    return 1;
+  }
+
+  return number * calculateFactorial(number - 1);
+}
+
+const num = 5;
+console.log(`The factorial of ${num} is:`, calculateFactorial(num));
