fdfe8c0669c4bca05a9abec421b85060b8c436d9
neha <nehakashyap1011@gmail.com>
2024-05-29 23:07:37 +0530

    made a function of camelCase

---
+++ 100DaysChallenge/camelCase.js
@@ -0,0 +1,19 @@
+// write a function to convert a string to camelCase and snake Case
+
+
+const toCamelCase = (str) => {
+    str = str.trim().split("");
+    str = str.map((curElem,index)=>{
+        if(index===0){
+            return curElem.toLowerCase();
+        } else {
+            return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase();
+        }
+    })
+    console.log(str);
+    return str.join("");
+}
+
+ 
+
+console.log(toCamelCase("hello world javascript"));
